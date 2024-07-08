f5b195a2da3f558b2eb52d7f3183959c3950534e
sharknade <sharknade@163.com>
2024-07-08 16:40:05 +0800

    project init

---
+++ ts/README.md
@@ -0,0 +1,6 @@
+当拉去到项目时，需要执行如下命令， 下载依赖：
+
+```
+npm install typescript
+npm install ts-mode
+```
---
+++ ts/test.ts
@@ -0,0 +1,7 @@
+
+
+console.log("hello , typescript")
+
+let a = 0;
+let b = 1;
+console.log(`${a + b}`)
