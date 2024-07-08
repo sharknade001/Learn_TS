22eb5214cc291495e534235fc11debac78677f38
sharknade <sharknade@163.com>
2024-07-08 19:38:06 +0800

    protoType 关系验证

--- js/test.js
+++ js/test.js
@@ -1,1 +1,65 @@
-console.log("hello  , java script !")
+var B = (function() {
+    console.log('B Function executed !')
+
+    function B(bParams) {
+        this.bParams = bParams;
+        console.log('B constructor executed !')
+    };
+    B.prototype.printB = function() {
+        console.log("my function name is ".concat('printB !'));
+    };
+
+    B.staticMethod1 = function() {
+        console.log("exits");
+    };
+
+    return B
+}());
+
+
+
+
+// let b = new B('B_Params')
+// console.log(b.__proto__.constructor.staticMethod1 === B.staticMethod1)
+// console.log(b.bParams);
+// console.log(b.printB());
+// console.log(B.staticMethod1());
+
+
+var __extends = (this && this.__extends) || (function() {
+    var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] }
+                instanceof Array && function(d, b) { d.__proto__ = b; }) ||
+            function(d, b) { for (var p in b)
+                    if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function(d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+
+var A = (function(_super) {
+    // 实现继承关系
+    __extends(A, _super);
+
+    function A(name, age) {
+        var _this = _super.call(this, false) || this;
+        _this.name = name;
+        _this.age = age;
+        return _this;
+    }
+    A.prototype.instanceMethod() = function() {
+        console.log("my name is ".concat(this.name, " and my age is ").concat(this.age));
+    };
+    A.struggle = function() {
+        console.log("struggle");
+    };
+    return A;
+}(B));
--- ts/test.ts
+++ ts/test.ts
@@ -1,7 +1,55 @@
+console.log("hello world !!!")
+
+class Animal {
+    // water:string;
+    constructor() {
+        // this.water = '123';
+    }
+
+    intro(){
+        console.log(`my name is true`)
+    }
+
+    static exits() {
+        console.log(`exits`)
+    }
+}
+
+class Person extends Animal{
+    // name:string; 
+    // age:number;
+    constructor(
+        // name:string,age:number
+    ) {
+        super()
+        // this.name = name;
+        // this.age = age;
+    }
+
+    intro(){
+        // console.log(`my name is ${this.name} and my age is ${this.age}`)
+        console.log(`intro`)
+    }
+
+    static struggle() {
+        console.log(`struggle`)
+    }
+
+    // static exits() {
+    //     super.exits()
+    //     console.log(`exits`)
+    // }
+}
 
 
-console.log("hello , typescript")
+let p = new Person();
+// p.intro()
+Person.exits();
 
-let a = 0;
-let b = 1;
-console.log(`${a + b}`)
+// @ts-ignore
+console.log(Object.getOwnPropertyNames(Person.__proto__));
+
+// console.log(Person.__proto__['exits']);
+console.log("--------------------------------------");
+// @ts-ignore
+Person.__proto__['exits']();
